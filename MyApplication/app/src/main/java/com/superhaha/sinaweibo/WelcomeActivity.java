ed3983a7397b6f65bdd3605f47e2890697cd749f
zhangyouwei <565393394@qq.com>
2017-09-04 17:37:59 +0800

    创建weibo项目

---
+++ MyApplication/app/src/main/java/com/superhaha/sinaweibo/WelcomeActivity.java
@@ -0,0 +1,18 @@
+package com.superhaha.sinaweibo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * 程序入口
+ * Created by superhaha on 2017/9/4.
+ */
+
+public class WelcomeActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
